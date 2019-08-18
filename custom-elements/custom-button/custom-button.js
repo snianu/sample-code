f8d4841974e3da63fe7e347cb514bd535d643d15
Bo Cupp <bo@seattlecupps.com>
2019-08-17 17:36:19 -0700

    Add custom-buton sample

---
+++ custom-elements/custom-button/custom-button.js
@@ -0,0 +1,27 @@
+// import {style} from "./custom-button.css"
+
+let css = `
+:host {
+	border: 3px solid gray;
+	border-style: outset;
+	background-color: #eee;
+	padding: 3px;
+	user-select: none;
+}
+
+:host:hover:active {
+	border-style: inset;
+}
+`
+class CustomButton extends HTMLElement {
+	constructor() {
+		super()
+
+		this.shadow = this.attachShadow({mode:"closed"})
+		// this.shadow.adoptedStylesheets = [style]
+
+		this.shadow.append(document.createElement("style"))
+		this.shadow.firstChild.append(css)
+	}
+}
+
